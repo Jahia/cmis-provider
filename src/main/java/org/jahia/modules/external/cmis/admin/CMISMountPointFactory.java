1524fb65185a89729ee804442f7c82370eacaaa4
kevan <kevan@jahia.com>
2014-11-25 11:23:36 +0100

    BACKLOG-5418: factory for CMIS and improve the abstraction layer to handle the query to retrieve folders since it's used by all the factories

---
+++ src/main/java/org/jahia/modules/external/cmis/admin/CMISMountPointFactory.java
@@ -0,0 +1,152 @@
+/**
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *     Rooted in Open Source CMS, Jahia’s Digital Industrialization paradigm is about
+ *     streamlining Enterprise digital projects across channels to truly control
+ *     time-to-market and TCO, project after project.
+ *     Putting an end to “the Tunnel effect”, the Jahia Studio enables IT and
+ *     marketing teams to collaboratively and iteratively build cutting-edge
+ *     online business solutions.
+ *     These, in turn, are securely and easily deployed as modules and apps,
+ *     reusable across any digital projects, thanks to the Jahia Private App Store Software.
+ *     Each solution provided by Jahia stems from this overarching vision:
+ *     Digital Factory, Workspace Factory, Portal Factory and eCommerce Factory.
+ *     Founded in 2002 and headquartered in Geneva, Switzerland,
+ *     Jahia Solutions Group has its North American headquarters in Washington DC,
+ *     with offices in Chicago, Toronto and throughout Europe.
+ *     Jahia counts hundreds of global brands and governmental organizations
+ *     among its loyal customers, in more than 20 countries across the globe.
+ *
+ *     For more information, please visit http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2014 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms & Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ *
+ *
+ *
+ *
+ */
+package org.jahia.modules.external.cmis.admin;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.jahia.modules.external.admin.mount.AbstractMountPointFactory;
+import org.jahia.modules.external.admin.mount.validator.LocalJCRFolder;
+import org.jahia.services.content.JCRNodeWrapper;
+
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+
+/**
+ * @author kevan
+ */
+public class CMISMountPointFactory extends AbstractMountPointFactory{
+    private static final long serialVersionUID = 2927976149191746013L;
+    protected static final String REPOSITORY_ID = "repositoryId";
+    protected static final String USER = "user";
+    protected static final String PASSWORD = "password";
+    protected static final String URL = "url";
+
+    @NotEmpty
+    private String name;
+    @LocalJCRFolder
+    private String localPath;
+    @NotEmpty
+    private String repositoryId;
+    private String user;
+    private String password;
+    @NotEmpty
+    private String url;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getLocalPath() {
+        return localPath;
+    }
+
+    @Override
+    public String getMountNodeType() {
+        return "cmis:cmisMountPoint";
+    }
+
+    @Override
+    public void setProperties(JCRNodeWrapper mountNode) throws RepositoryException {
+        mountNode.setProperty(REPOSITORY_ID, repositoryId);
+        mountNode.setProperty(USER, user);
+        mountNode.setProperty(PASSWORD, password);
+        mountNode.setProperty(URL, url);
+    }
+
+    @Override
+    public void populate(JCRNodeWrapper nodeWrapper) throws RepositoryException {
+        super.populate(nodeWrapper);
+        this.name = getName(nodeWrapper.getName());
+        try {
+            this.localPath = nodeWrapper.getProperty("mountPoint").getNode().getPath();
+        }catch (PathNotFoundException e) {
+            // no local path defined for this mount point
+        }
+        this.repositoryId = nodeWrapper.getPropertyAsString(REPOSITORY_ID);
+        this.user = nodeWrapper.getPropertyAsString(USER);
+        this.password = nodeWrapper.getPropertyAsString(PASSWORD);
+        this.url = nodeWrapper.getPropertyAsString(URL);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocalPath(String localPath) {
+        this.localPath = localPath;
+    }
+
+    public String getRepositoryId() {
+        return repositoryId;
+    }
+
+    public void setRepositoryId(String repositoryId) {
+        this.repositoryId = repositoryId;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/org/jahia/modules/external/cmis/admin/CMISMountPointFactoryHandler.java
@@ -0,0 +1,106 @@
+/**
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *     Rooted in Open Source CMS, Jahia’s Digital Industrialization paradigm is about
+ *     streamlining Enterprise digital projects across channels to truly control
+ *     time-to-market and TCO, project after project.
+ *     Putting an end to “the Tunnel effect”, the Jahia Studio enables IT and
+ *     marketing teams to collaboratively and iteratively build cutting-edge
+ *     online business solutions.
+ *     These, in turn, are securely and easily deployed as modules and apps,
+ *     reusable across any digital projects, thanks to the Jahia Private App Store Software.
+ *     Each solution provided by Jahia stems from this overarching vision:
+ *     Digital Factory, Workspace Factory, Portal Factory and eCommerce Factory.
+ *     Founded in 2002 and headquartered in Geneva, Switzerland,
+ *     Jahia Solutions Group has its North American headquarters in Washington DC,
+ *     with offices in Chicago, Toronto and throughout Europe.
+ *     Jahia counts hundreds of global brands and governmental organizations
+ *     among its loyal customers, in more than 20 countries across the globe.
+ *
+ *     For more information, please visit http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2014 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms & Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ *
+ *
+ *
+ *
+ */
+package org.jahia.modules.external.cmis.admin;
+
+import org.jahia.modules.external.admin.mount.AbstractMountPointFactoryHandler;
+import org.jahia.services.content.JCRCallback;
+import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.content.JCRTemplate;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.webflow.execution.RequestContext;
+
+import javax.jcr.RepositoryException;
+
+/**
+ * Created by kevan on 25/11/14.
+ */
+public class CMISMountPointFactoryHandler extends AbstractMountPointFactoryHandler<CMISMountPointFactory>{
+    private static final long serialVersionUID = -1825547537625217141L;
+    private static final Logger logger = LoggerFactory.getLogger(CMISMountPointFactoryHandler.class);
+
+    private CMISMountPointFactory cmisMountPointFactory;
+
+    public void init(RequestContext requestContext) {
+        cmisMountPointFactory = new CMISMountPointFactory();
+        try {
+            super.init(requestContext, cmisMountPointFactory);
+        } catch (RepositoryException e) {
+            logger.error("Error retrieving mount point", e);
+        }
+        requestContext.getFlowScope().put("cmisFactory", cmisMountPointFactory);
+    }
+
+    public String getFolderList() {
+        JSONObject result = new JSONObject();
+        try {
+            JSONArray folders = JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<JSONArray>() {
+                @Override
+                public JSONArray doInJCR(JCRSessionWrapper session) throws RepositoryException {
+                    return getSiteFolders(session.getWorkspace());
+                }
+            });
+
+            result.put("folders", folders);
+        } catch (RepositoryException e) {
+            logger.error("Error trying to retrieve local folders", e);
+        } catch (JSONException e) {
+            logger.error("Error trying to construct JSON from local folders", e);
+        }
+
+        return result.toString();
+    }
+
+    public Boolean save() {
+        try {
+            return super.save(cmisMountPointFactory);
+        } catch (RepositoryException e) {
+            logger.error("Error saving mount point " + cmisMountPointFactory.getName(), e);
+        }
+        return false;
+    }
+}
