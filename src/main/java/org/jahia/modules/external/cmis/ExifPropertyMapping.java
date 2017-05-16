0b4a60c8f3517d36e2aa7992d76f3a50afb66227
bpapez <bpapez@jahia.com>
2017-05-16 19:08:51 +0200

    QA-8059: Add missing copyright header

--- src/main/java/org/jahia/modules/external/cmis/ExifPropertyMapping.java
+++ src/main/java/org/jahia/modules/external/cmis/ExifPropertyMapping.java
@@ -1,10 +1,32 @@
+/**
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *                                  http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2016 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms & Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ */
 package org.jahia.modules.external.cmis;
 
 /**
- * Created by rahmed on 12/05/17.
+ * Configuration class to map EXIF metadata based CMIS <-> JCR type attributes.
  */
 public class ExifPropertyMapping extends CmisPropertyMapping {
-    //Class for exif properties to be check on instance
 
     public ExifPropertyMapping() {
         super();
