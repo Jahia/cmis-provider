a59070f4862a8b8cf79f22f5eb772a14ef7bdb0d
akarmanov <alexandrk_@hotmail.com>
2023-03-23 14:42:20 -0400

    TECH-1109 Set proxy url

--- tests/cypress/plugins/env.ts
+++ tests/cypress/plugins/env.ts
@@ -2,5 +2,6 @@ module.exports = (on, config) => {
     config.baseUrl = process.env.JAHIA_URL;
     config.env.AS_SITEKEY = 'digitall';
     config.env.ALFRESCO_URL = process.env.ALFRESCO_URL;
+    config.env.PROXY_URL = process.env.PROXY_URL;
     return config
 }
